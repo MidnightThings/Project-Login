2b0beac7a96931ef787a44bd236c65c1b2577c60
Choreas <niklas.kosanke@web.de>
2021-01-19 16:19:02 +0100

    stub login form

--- app/index.php
+++ app/index.php
@@ -1,8 +1,24 @@
-<html>
-	<body>
-	<?php 
-		echo 'test';
-	?>
-	
-	</body>
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginWorkflow</title>
+  </head>
+  <body>
+		<div>
+			<form 
+				method="POST" 
+				action="login/loginFormSubmit.php"
+			>
+				<label for="inputUsername">Username</label>
+				<input type="text" name="inputUsername" title="Username" id="inputUsername" />
+				<br>
+				<label for="inputPassword">Password</label>
+				<input type="password" name="inputPassword" title="Password" id="inputPassword" />
+				<br>
+				<input type="submit"/>
+			</form>
+		</div>
+  </body>
 </html>
---
+++ app/login/loginFormSubmit.php
@@ -0,0 +1,5 @@
+<?php
+  $pw = $_POST["inputPassword"];
+  $username = $_POST["inputUsername"];
+  echo $pw . "<br>" . $username;
+?>
